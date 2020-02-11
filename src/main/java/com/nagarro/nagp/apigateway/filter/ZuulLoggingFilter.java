ffa78a700bcbe4252931fa592d07a02eececf954
sahilsharma <sahil.sharma@nagarro.com>
2020-02-11 09:43:38 +0530

    initial commit

---
+++ src/main/java/com/nagarro/nagp/apigateway/filter/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.nagarro.nagp.apigateway.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("Request :-> {}", request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
